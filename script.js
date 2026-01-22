9e3a8cb475f659acde3a0d5a1250f42cd850ed03
SERI-arc <yserita@dl-techx-xedge.net>
2026-01-22 18:19:06 +0900

    セッション管理機能追加

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Axios共通設定
 // ==============================
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "m3hserita-backapp.m3harbor.net/api/api"; // バックエンドのカスタムドメイン
+axios.defaults.baseURL = "https://m3hserita0122rec.m3harbor.net/api"; // バックエンドのカスタムドメイン
 
 // ==============================
 // Vueアプリ設定
