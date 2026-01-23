71a5399aa6d2409da29ebffc6c2e3d806f2eecf9
SERI-arc <yserita@dl-techx-xedge.net>
2026-01-23 12:48:26 +0900

    資源のデプロイ

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Axios共通設定
 // ==============================
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "https://m3hserita0122rec.m3harbor.net/api"; // バックエンドのカスタムドメイン
+axios.defaults.baseURL = "https://m3hserita-backapp.m3harbor.net/api"; // バックエンドのカスタムドメイン
 
 // ==============================
 // Vueアプリ設定
